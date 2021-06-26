99bbc83d16b0cc228b3fafd8d85a45656c45dec9
elevyg <elevyg91@gmail.com>
2021-06-26 14:45:43 -0400

    feat: absolute imports and i18next

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 module.exports = {
   siteMetadata: {
     pathPrefix: "/huerto-jeinimeni",
@@ -7,17 +10,27 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          // src: path.join(__dirname, "src"),
+          // pages: path.join(__dirname, "src/pages"),
+          "@components": "src/components",
+          // locales: path.join(__dirname, "locale"),
+        },
+        extensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-emotion`,
-    `gatsby-plugin-root-import`,
     `gatsby-plugin-smoothscroll`,
     {
       resolve: "gatsby-plugin-anchor-links",
       options: {
-        duration: 1000
-      }
-      
+        duration: 1000,
+      },
     },
     {
       resolve: "gatsby-plugin-eslint",
@@ -56,5 +69,42 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/locales`,
+        name: `locale`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
+        languages: [`en`, `es`],
+        defaultLanguage: `en`,
+        // if you are using Helmet, you must include siteUrl, and make sure you add http:https
+        siteUrl: `https://chilechico.live/`,
+        // you can pass any i18next options
+        // pass following options to allow message content as a key
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+          },
+          keySeparator: false,
+          nsSeparator: false,
+        },
+        pages: [
+          {
+            matchPath: "/:lang?/blog/:uid",
+            getLanguageFromPath: true,
+            excludeLanguages: ["es"],
+          },
+          {
+            matchPath: "/preview",
+            languages: ["en"],
+          },
+        ],
+      },
+    },
   ],
 };
